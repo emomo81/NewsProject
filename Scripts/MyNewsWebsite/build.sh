29a47d9e75bd1ac348eccb983a46eba9d17477f3
emomo81 <emmanuelmomo81@gmail.com>
2026-03-17 22:30:12 +0200

    fix: remove migrate from build step

--- Scripts/MyNewsWebsite/build.sh
+++ Scripts/MyNewsWebsite/build.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Build Frontend
 echo "Building Frontend..."
 cd ../../frontend
 npm install
 npm run build
 cd ../Scripts/MyNewsWebsite
 
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --no-input
-# Remove the migrate and create_superuser lines
