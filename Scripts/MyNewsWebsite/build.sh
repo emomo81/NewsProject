18ebeed56da53e0c58598fe13bd347e2bdeeed4e
emomo81 <emmanuelmomo81@gmail.com>
2026-03-17 22:22:07 +0200

    fix: move migrations to start command

--- Scripts/MyNewsWebsite/build.sh
+++ Scripts/MyNewsWebsite/build.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# Build Frontend
 echo "Building Frontend..."
 cd ../../frontend
 npm install
 npm run build
 cd ../Scripts/MyNewsWebsite
 
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
 python manage.py collectstatic --no-input
-
-# Apply database migrations
-python manage.py migrate
-
-# Create superuser if needed
-python create_superuser.py
+# Remove the migrate and create_superuser lines
